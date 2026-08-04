2d6edc6cf35c8a4c8536843fd7fbb20f43d468d0
shreya <shreya9352@gmail.com>
2026-08-04 17:51:04 +0530

    703 Leetcode Question

--- SoluMain.java
+++ SoluMain.java
@@ -1,4 +1,8 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 class Solution {
+
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
@@ -16,5 +20,19 @@ class Solution {
         }
 
         return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
+
+public class SoluMain {
+
+    public static void main(String[] args) {
+
+        int[] stones = {2, 7, 4, 1, 8, 1};
+
+        Solution obj = new Solution();
+
+        int result = obj.lastStoneWeight(stones);
+
+        System.out.println(result);
     }
 }
